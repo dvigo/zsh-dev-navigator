4cff8449bdfba01ed2914d885c2e5910e30fd703
David Vigo <david.vigoesteban@gmail.com>
2025-10-04 00:13:30 +0200

    ✨ Feature: Add option to open root dev folder via fzf

--- zsh-dev-navigator.plugin.zsh
+++ zsh-dev-navigator.plugin.zsh
@@ -20,7 +20,7 @@ dev() {
     if [ -z "$project_name" ]; then
         if [[ -d "$DEV_BASE_DIR" ]]; then
             if command -v fzf >/dev/null 2>&1; then
-                project_name=$(ls -1 "$DEV_BASE_DIR" | fzf --height 40% --border --prompt="Select project: ")
+                project_name=$(ls -1 "$DEV_BASE_DIR" | fzf --height 40% --border --prompt="Select project: " --bind "ctrl-c:abort" --header="Press ESC to open root dev folder")
             fi
         else
             echo "DEV base directory not found: $DEV_BASE_DIR" >&2
